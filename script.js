e96a62efff1cd8cf3bc81420f44f79baea4b93ef
Sindhu K P <107877975+SindhuKP23@users.noreply.github.com>
2026-01-28 16:23:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Message sent successfully!");
+  form.reset();
+});
